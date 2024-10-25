86c83f9c77ec4eb72b77cbd77e918ed106da758c
artemgon <artemhoncharuk123@gmail.com>
2024-10-25 22:53:28 +0300

    Clearing developer.

--- main.cpp
+++ main.cpp
@@ -1,15 +1,7 @@
-#include "common-project-class-s.h"
+#include <iostream>
+using namespace std;
 
 int main()
 {
-	Product newProduct("Guitar", 1400, 10);
-	newProduct.display();
-
-	DiscountedProduct newDiscountedProduct("Electric Bass Guitar", 2300, 10, 15);
-	newDiscountedProduct.display();
-
-	Product* productPtr = &newDiscountedProduct;
-	productPtr->display();
-
 	return 0;
 }
