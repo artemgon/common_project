da08a12703283d9ebadf58f624e4cb18a8ad84dd
artemgon <artemhoncharuk123@gmail.com>
2024-10-25 23:14:45 +0300

    Editing classes and main.

--- common-project-class-s.h
+++ common-project-class-s.h
@@ -5,96 +5,98 @@
 class Product
 {
 protected:
-	string name;
-	float price;
-	int quantity;
+    string name;
+    double price;
+    int quantity;
 
 public:
-	Product(const string name, double price, int quantity) : name(name), price(price), quantity(quantity) {}
+    Product(const string name, double price, int quantity) : name(name), price(price), quantity(quantity) {}
 
-	virtual ~Product() = default;
+    virtual ~Product() = default;
 
-	virtual float getPrice() const
-	{
-		return price;
-	}
-	virtual int getQuantity() const
-	{
-		return quantity;
-	}
-	virtual void updatePrice(double newPrice)
-	{
-		price = newPrice;
-	}
-	virtual void updateQuantity(int newQuantity)
-	{
-		quantity = newQuantity;
-	}
-	virtual void display() const
-	{
-		cout << "Name: " << name << endl;
-		cout << "Price: " << price << "$" << endl;
-		cout << "Quantity: " << quantity << "\n" << endl;
-	}
-	
+    virtual string getName() const
+    {
+        return name;
+    }
+    virtual double getPrice() const
+    {
+        return price;
+    }
+    virtual int getQuantity() const
+    {
+        return quantity;
+    }
+    virtual void updatePrice(double newPrice)
+    {
+        price = newPrice;
+    }
+    virtual void updateQuantity(int newQuantity)
+    {
+        quantity = newQuantity;
+    }
+
+
+    virtual string GetInfo() const
+    {
+        return "Product: " + name + "\nPrice: $" + to_string(price) + "\nQuantity: " + to_string(quantity) + "\n";
+    }
 };
 
-class Category
-{
-string categoryName, *categories;
-Product* products;
-int size_1, size_2;
-public:
-	void addProduct(int index, const string& name, float price, int quantity)
-	{
-		if (index >= 0 && index < size_1)
-			products[index] = Product(name, price, quantity);
-	}
-	void removeProduct(int index) {
-		if (index < 0 || index >= size_1) {
-			cout << "Invalid index!" << endl;
-		}
-		for (int i = index; i < size_1 - 1; ++i) {
-			products[i] = products[i + 1];
-		}
-		size_1--;
-		cout << "Item removed from index " << index << ".\n";
-	}
-	void getProducts(int products)
-	{
-		for (int i = 0; i < size_1; ++i)
-		{
-			cout << products[i] << " ";
-		}
-	}
-	~Category() {
-		delete[] products;
-	}
-};
 
 class DiscountedProduct : public Product
 {
 private:
-	float discount;
+    double discount;
 public:
-	DiscountedProduct(const string name, float price, int quantity, float discount) : Product(name, price, quantity), discount(discount) {}
+    DiscountedProduct(const string name, double price, int quantity, double discount) : Product(name, price, quantity), discount(discount) {}
 
-	float getPrice() const override
-	{
-		 return price * (1 - discount / 100);
-	}
+    double getPrice() const override
+    {
+        return price * (1 - discount / 100.0);
+    }
 
-	void setDiscount(float newDiscount)
-	{
-		discount = newDiscount;
-	}
-	void display() const override
-	{
-		cout << "Name(discounted): " << name << endl;
-		cout << "Start Price:" << price <<  "$" << endl;
-		cout << "Discount: " << discount << "%" << endl;
-		cout << "Quantity: " << quantity << endl;
-		cout << "Discounted Price: " << discount << "$" << "\n" << endl;
-	}
+    void setDiscount(double newDiscount)
+    {
+        discount = newDiscount;
+    }
+    string GetInfo() const override
+    {
+        return "Product: " + name + "\nPrice: $" + to_string(price) + "\nQuantity: " + to_string(quantity) + "\nDiscount: " + to_string(discount) + "\nDiscounted price: " + to_string(getPrice()) + "%\n";
+    }
+};
+
+class Category
+{
+private:
+    string name;
+    vector<shared_ptr<Product>> products;
+public:
+    Category(const string name) : name(name) {}
+
+    void addProduct(const shared_ptr<Product>& product)
+    {
+        products.push_back(product);
+    }
+
+    void removeProduct(const string& productName)
+    {
+        products.erase(remove_if(products.begin(), products.end(), [&productName](const shared_ptr<Product>& product) {
+            return product->getName() == productName;
+            }), products.end());
+
+    }
+    void listProducts() const
+    {
+        cout << "Category: " << name << endl;
+        for (const auto& product : products)
+        {
+            product->GetInfo();
+        }cout << endl;
+
+    }
+    int getProductCount() const
+    {
+        return products.size();
+    }
 };
 #endif // common_proj_s
--- main.cpp
+++ main.cpp
@@ -3,13 +3,28 @@
 int main()
 {
 	Product newProduct("Guitar", 1400, 10);
-	newProduct.display();
+	Category newCategory("Musical Instruments");
+	newProduct.GetInfo();
+
+	shared_ptr<Product> newProductPtr = make_shared<Product>("Drums", 2000, 5);
+	newCategory.addProduct(newProductPtr);
+	shared_ptr<Product> newProductPtr2 = make_shared<Product>("Piano", 3000, 3);
+	newCategory.addProduct(newProductPtr2);
+	shared_ptr<Product> newProductPtr3 = make_shared<Product>("Electric Bass Guitar", 2300, 10);
+	newCategory.addProduct(newProductPtr3);
+	shared_ptr<Product> newDIscountedProductPtr1 = make_shared<Product>("Electric Bass Guitar", 2300, 10);
+	newCategory.addProduct(newDIscountedProductPtr1);
 
 	DiscountedProduct newDiscountedProduct("Electric Bass Guitar", 2300, 10, 15);
-	newDiscountedProduct.display();
+	newDiscountedProduct.GetInfo();
+	newCategory.listProducts();
+	newCategory.removeProduct("Piano");
+
+	cout << "Product count:" << newCategory.getProductCount() << endl;
+
 
 	Product* productPtr = &newDiscountedProduct;
-	productPtr->display();
+	productPtr->GetInfo();
 
 	return 0;
 }
